144a1e2025b79cd8dc67e1751c92867267906897
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-02-26 15:44:14 +0100

    :sparkles: Adds the start code

--- client/vite-project/main.js
+++ client/vite-project/main.js
@@ -1,24 +1,22 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { io } from 'socket.io-client'; 
+const socket = io('http://localhost:3001');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let sendMessage = document.getElementById('sendMessage');
+let sendBtn = document.getElementById('sendBtn');
+let chatList = document.getElementById('chatList'); 
 
-setupCounter(document.querySelector('#counter'))
+sendBtn.addEventListener('click', () => {
+  console.log('send chat', sendMessage.value);
+  socket.emit('chat', sendMessage.value); 
+})
+
+socket.on('chat', (arg) => {
+  console.log('socket', arg);
+  updateChat(arg);
+})
+
+function updateChat(chat) {
+  let li = document.createElement('li'); 
+  li.innerText = chat; 
+  chatList.appendChild(li); 
+}
