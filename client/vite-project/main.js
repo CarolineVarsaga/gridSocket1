9ea9d8942507adf56b7da2623e1ffa58e2c13cfa
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-02-27 15:30:53 +0100

    :sparkles: Adds sent chat to right and recieved to the left

--- client/vite-project/main.js
+++ client/vite-project/main.js
@@ -4,19 +4,35 @@ const socket = io('http://orca-app-rblx6.ondigitalocean.app');
 let sendMessage = document.getElementById('sendMessage');
 let sendBtn = document.getElementById('sendBtn');
 let chatList = document.getElementById('chatList'); 
+let myName = "User";
 
 sendBtn.addEventListener('click', () => {
   console.log('send chat', sendMessage.value);
-  socket.emit('chat', sendMessage.value); 
+  socket.emit('chat', { message: sendMessage.value, sender: myName }); //skickar meddelande
+  updateChat(sendMessage.value, 'sent');
+  sendMessage.value = '';
 })
 
 socket.on('chat', (arg) => {
   console.log('socket', arg);
-  updateChat(arg);
+  if (arg.sender !== myName) {
+    updateChat(arg, 'received');
+  }
+  
 })
 
-function updateChat(chat) {
+function updateChat(chat, sender) {
   let li = document.createElement('li'); 
   li.innerText = chat; 
-  chatList.appendChild(li); 
+  let div = document.createElement('div');
+  //div.classList.add('li-container')
+  if (sender === 'sent') {
+    li.classList.add('sent')
+    div.classList.add('sent-container')
+  } else {
+    li.classList.add('received')
+    div.classList.add('received-container')
+  }
+  div.appendChild(li);
+  chatList.appendChild(div)
 }
