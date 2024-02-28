31436d37e58ee95ee66d0cd0bd5d8dd8858441b2
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-02-28 16:40:06 +0100

    :sparkles: Adds log in

--- client/vite-project/main.js
+++ client/vite-project/main.js
@@ -4,20 +4,34 @@ const socket = io('http://orca-app-rblx6.ondigitalocean.app');
 let sendMessage = document.getElementById('sendMessage');
 let sendBtn = document.getElementById('sendBtn');
 let chatList = document.getElementById('chatList'); 
-let myName = "User";
+let myName = localStorage.getItem('user');
+
+//================================================
+//==================   LOG IN   ==================
+//================================================
+const nameInput = document.getElementById('nameInput');
+const joinBtn = document.getElementById('joinBtn');
+
+joinBtn.addEventListener('click', () => {
+  localStorage.getItem('user');
+  let user = nameInput.value; 
+  localStorage.setItem('user', user);
+  nameInput.value = ''; 
+})
 
 
+//=================================================
 sendBtn.addEventListener('click', () => {
-  let messageObject = { message: sendMessage.value, sender: myName }
+  let messageObject = { message: sendMessage.value, sender: myName }  
   console.log('send chat', sendMessage.value);
+  console.log('sender', messageObject.sender)
   socket.emit('chat', messageObject.message); //skickar meddelande
-  socket.emit
   updateChat(sendMessage.value, 'sent');
   sendMessage.value = '';
 })
 
 socket.on('chat', (arg) => {
-  console.log('main.js - socket', arg);
+  console.log('main.js - socket', arg); 
   if (arg.sender !== myName) {
     updateChat(arg, 'received');
   }  
@@ -27,14 +41,32 @@ function updateChat(chat, sender) {
   let li = document.createElement('li'); 
   li.innerText = chat; 
   let div = document.createElement('div');
+  let name = document.createElement('p');
+  
   //div.classList.add('li-container')
   if (sender === 'sent') {
     li.classList.add('sent')
     div.classList.add('sent-container')
+    name.innerText = myName;
   } else {
     li.classList.add('received')
     div.classList.add('received-container')
   }
   div.appendChild(li);
-  chatList.appendChild(div)
+  div.appendChild(name);
+  chatList.appendChild(div);
+
+  scrollToBottom(); 
 } 
+
+//================================================
+//==============   CHAT BOX SCROLL   =============
+//================================================
+function scrollToBottom() {
+  const chatBox = document.getElementById('chatBox');
+  chatBox.scrollTop = chatBox.scrollHeight; 
+}
+
+
+
+
